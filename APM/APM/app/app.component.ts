c404d88fe8fae077679ad14199cdd1a9eb71ef06
test <esgfsf@wefwfe.com>
2016-11-30 15:51:20 +0100

    Added ProductList Component

--- APM/APM/app/app.component.ts
+++ APM/APM/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
     selector: 'pm-app',
     template: `
-    <div>
-        <h1>{{pageTitle}}</h1>
-        <pm-products>Loading Products...</pm-products>
-    </div>
-    `
+        <div>
+            <h1>{{pageTitle}}</h1>
+            <pm-products>Loading Products...</pm-products>
+        </div>
+        `,
+    providers: [ProductService]
 })
 export class AppComponent {
     pageTitle = 'Acme Product Management';
